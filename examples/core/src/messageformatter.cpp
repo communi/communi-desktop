abf159ae529cc6c51ebe59b6ee1e9a44cc123ba5
J-P Nurmi <jpnurmi@gmail.com>
2011-08-27 00:51:45 +0200

    MessageFormatter: implemented formatting for most of numeric messages

--- examples/core/src/messageformatter.cpp
+++ examples/core/src/messageformatter.cpp
@@ -129,10 +129,68 @@ QString MessageFormatter::formatNoticeMessage(IrcNoticeMessage* message) const
     return tr("[%1] %2").arg(prefix, msg);
 }
 
+#define P_(x) message->parameters().value(x)
+
 QString MessageFormatter::formatNumericMessage(IrcNumericMessage* message) const
 {
-    const QString prefix = prettyUser(message->prefix());
-    return tr("# %1 %2 %3").arg(prefix, message->command(), message->parameters().join(" "));
+    if (message->code() < 300)
+        return tr("[INFO] %1").arg(QStringList(message->parameters().mid(1)).join(" "));
+    if (message->code() > 399)
+        return tr("[ERROR] %1").arg(QStringList(message->parameters().mid(1)).join(" "));
+
+    switch (message->code())
+    {
+    case Irc::RPL_MOTDSTART:
+    case Irc::RPL_MOTD:
+        return tr("[MOTD] %1").arg(QStringList(message->parameters().mid(1)).join(" "));
+    case Irc::RPL_AWAY:
+    case Irc::RPL_WHOISOPERATOR:
+    case 310: // "is available for help"
+    case 320: // "is identified to services"
+    case 378: // nick is connecting from <...>
+    case 671: // nick is using a secure connection
+        return tr("! %1 %2").arg(message->prefix(), message->parameters().join(" "));
+    case Irc::RPL_WHOISUSER:
+        return tr("! %1 is %2@%3 %4").arg(P_(1), P_(2), P_(3), P_(4));
+    case Irc::RPL_WHOISSERVER:
+        return tr("! %1 online via %2 (%3)").arg(P_(1), P_(2), P_(3));
+    case 330: // nick user is logged in as
+        return tr("! %1 %3 %2").arg(P_(1), P_(2), P_(3));
+    case Irc::RPL_WHOWASUSER:
+        return tr("! %1 was %2@%3 %4 %5").arg(P_(1), P_(2), P_(3), P_(4), P_(5));
+    case Irc::RPL_WHOISIDLE: {
+        QDateTime signon = QDateTime::fromTime_t(P_(3).toInt());
+        QTime idle = QTime().addSecs(P_(2).toInt());
+        return tr("! %1 has been online since %2 (idle for %3)").arg(P_(1), signon.toString(), idle.toString());
+    }
+    case Irc::RPL_WHOISCHANNELS:
+        return tr("! %1 is on channels %2").arg(P_(1), P_(2));
+    case Irc::RPL_CHANNELMODEIS:
+        return tr("! %1 mode is %2").arg(P_(1), P_(2));
+    case Irc::RPL_CHANNELURL:
+        return tr("! %1 url is %2").arg(P_(1), P_(2));
+    case Irc::RPL_CHANNELCREATED: {
+        QDateTime dateTime = QDateTime::fromTime_t(P_(2).toInt());
+        return tr("! %1 was created %2").arg(P_(1), dateTime.toString());
+    }
+    case Irc::RPL_NOTOPIC:
+        return tr("! %1 has no topic set").arg(P_(1));
+    case Irc::RPL_TOPIC:
+        return tr("! %1 topic is \"%2\"").arg(P_(1), P_(2));
+    case Irc::RPL_TOPICSET: {
+        QDateTime dateTime = QDateTime::fromTime_t(P_(3).toInt());
+        return tr("! %1 topic was set %2 by %3").arg(P_(1), dateTime.toString(), P_(2));
+    }
+    case Irc::RPL_INVITING:
+        return tr("! %1 is inviting %1 to %2").arg(message->prefix(), P_(1), P_(2));
+    case Irc::RPL_VERSION:
+        return tr("! %1 version is %2").arg(message->prefix(), P_(1));
+    case Irc::RPL_TIME:
+        return tr("! %1 time is %2").arg(P_(1), P_(2));
+
+    default:
+        return tr("[%1] %2").arg(message->code()).arg(QStringList(message->parameters().mid(1)).join(" "));
+    }
 }
 
 QString MessageFormatter::formatPartMessage(IrcPartMessage* message) const
