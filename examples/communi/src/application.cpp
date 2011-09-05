2a201918fde623713d9cdeac9484963c5ac21593
J-P Nurmi <jpnurmi@gmail.com>
2011-09-05 20:02:58 +0200

    Updated license headers

--- examples/communi/src/application.cpp
+++ examples/communi/src/application.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2008-2010 J-P Nurmi jpnurmi@gmail.com
+* Copyright (C) 2008-2011 J-P Nurmi jpnurmi@gmail.com
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -10,12 +10,6 @@
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* $Id$
 */
 
 #include "application.h"
@@ -110,7 +104,7 @@ void Application::aboutApplication()
         "<h3>%1</h3>"
         "<p>%2</p>"
         "<p><a href='%3'>%3</a></p>"
-        "<p><small>Copyright (C) 2008-2010 J-P Nurmi &lt;"
+        "<p><small>Copyright (C) 2008-2011 J-P Nurmi &lt;"
         "<a href='mailto:jpnurmi@gmail.com'>jpnurmi@gmail.com</a>&gt;</small></p>")
             .arg(tr("About %1").arg(applicationName()))
             .arg(applicationSlogan())
--- examples/core/src/messageformatter.cpp
+++ examples/core/src/messageformatter.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2008-2010 J-P Nurmi jpnurmi@gmail.com
+* Copyright (C) 2008-2011 J-P Nurmi jpnurmi@gmail.com
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -10,12 +10,6 @@
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* $Id$
 */
 
 #include "messageformatter.h"
