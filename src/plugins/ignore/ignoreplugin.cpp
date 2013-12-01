ef423454ecc5ff898ab2bee4502a7b974c7ed2a2
J-P Nurmi <jpnurmi@gmail.com>
2013-12-01 18:58:56 +0100

    IgnorePlugin: add TODO notes

--- src/plugins/ignore/ignoreplugin.cpp
+++ src/plugins/ignore/ignoreplugin.cpp
@@ -17,6 +17,31 @@
 
 IgnorePlugin::IgnorePlugin(QObject* parent) : QObject(parent)
 {
+    // TODO:
+    // parser->addCommand(IrcCommand::Custom, "IGNORE (<mask>)");
+    // parser->addCommand(IrcCommand::Custom, "UNIGNORE <mask>");
+
+//    if (command == "IGNORE") {
+//        MessageFormatter::Options options;
+//        options.timeStampFormat = d.timeStampFormat;
+//        options.timeStamp = QDateTime::currentDateTime();
+//        QString ignore  = params.value(0);
+//        if (ignore.isEmpty()) {
+//            const QStringList ignores = IgnoreManager::instance()->ignores();
+//            if (!ignores.isEmpty()) {
+//                foreach (const QString& ignore, ignores)
+//                    d.textBrowser->append(MessageFormatter::formatLine("! " + ignore, options));
+//            } else {
+//                d.textBrowser->append(MessageFormatter::formatLine("! no ignores", options));
+//            }
+//        } else {
+//            QString mask = IgnoreManager::instance()->addIgnore(ignore);
+//            d.textBrowser->append(MessageFormatter::formatLine("! ignored: " + mask));
+//        }
+//    } else if (command == "UNIGNORE") {
+//        QString mask = IgnoreManager::instance()->removeIgnore(params.value(0));
+//        d.textBrowser->append(MessageFormatter::formatLine("! unignored: " + mask));
+//    }
 }
 
 void IgnorePlugin::initConnection(IrcConnection* connection)
