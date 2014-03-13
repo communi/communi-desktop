dc8026f39887f4bd13bb5f4fd73b4c1a67235c20
J-P Nurmi <jpnurmi@gmail.com>
2014-03-13 13:18:08 +0100

    Fix license headers to reduce diff to communi-desktop

--- src/libs/base/listview.h
+++ src/libs/base/listview.h
@@ -1,11 +1,16 @@
 /*
- * Copyright (C) 2008-2014 The Communi Project
- *
- * This example is free, and not covered by the LGPL license. There is no
- * restriction applied to their modification, redistribution, using and so on.
- * You can study them, modify them, use them in your own program - either
- * completely or partially.
- */
+* Copyright (C) 2008-2014 The Communi Project
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 #ifndef LISTVIEW_H
 #define LISTVIEW_H
