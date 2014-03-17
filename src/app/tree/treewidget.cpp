cd23d8c7cbdbb9526bc8b5b49afd1a517c1cde0e
J-P Nurmi <jpnurmi@gmail.com>
2014-03-18 00:00:03 +0100

    TreeWidget: cleanup old znc playback begin/end checks

--- src/app/tree/treewidget.cpp
+++ src/app/tree/treewidget.cpp
@@ -391,8 +391,7 @@ void TreeWidget::onMessageReceived(IrcMessage* message)
                 }
             }
             if (!visible) {
-                if (message->nick() != QLatin1String("***") || message->ident() != QLatin1String("znc"))
-                    item->setData(1, TreeRole::Badge, item->data(1, TreeRole::Badge).toInt() + 1);
+                item->setData(1, TreeRole::Badge, item->data(1, TreeRole::Badge).toInt() + 1);
                 if (message->property("private").toBool() ||
                         message->property("content").toString().contains(message->connection()->nickName(), Qt::CaseInsensitive)) {
                     highlightItem(item);
