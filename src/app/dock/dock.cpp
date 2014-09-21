35e2424b91c0301d1ab085a27efc4a1bbb2dbc4f
J-P Nurmi <jpnurmi@gmail.com>
2014-09-21 14:23:34 +0200

    Revert "Disable the whole system tray on Linux"
    With Qt 5.3, QSystemTrayIcon no longer creates an extra
    top level window on X11.
    
    This reverts commit 9c580ece66fe1dda6285909b167544f22d3a556b.

--- src/app/dock/dock.cpp
+++ src/app/dock/dock.cpp
@@ -44,7 +44,6 @@ Dock::Dock(MainWindow* window) : QObject(window)
     if (QtDockTile::isAvailable())
         d.dock = new QtDockTile(window);
 
-#if defined(Q_OS_MAC) || defined(Q_OS_WIN)
     if (QSystemTrayIcon::isSystemTrayAvailable()) {
         d.tray = new QSystemTrayIcon(this);
 
@@ -76,7 +75,6 @@ Dock::Dock(MainWindow* window) : QObject(window)
 
         updateTray();
     }
-#endif
 
     if (Alert::isAvailable()) {
         d.alert = new Alert(this);
@@ -98,9 +96,11 @@ void Dock::alert(IrcMessage* message)
         if (d.alert && (!d.mute || !d.mute->isChecked()))
             d.alert->play();
         if (d.tray && !d.blinking) {
+#if defined(Q_OS_MAC) || defined(Q_OS_WIN)
             QString content = message->property("content").toString();
             if (!content.isEmpty())
                 d.tray->showMessage(tr("Communi"), message->nick() + ": " + IrcTextFormat().toPlainText(content));
+#endif
             SharedTimer::instance()->registerReceiver(this, "updateTray");
             d.blinking = true;
             d.blink = true;
