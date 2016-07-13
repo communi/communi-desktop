fbc55c941e82779077622dac6c6f9df11ee36e62
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2016-07-13 17:35:11 +0200

    Revert "SplitView: restore the server buffer"
    This reverts commit f906793d72c51b38fefb5b185eb1b971aa89421f.
    
    The server buffer is restored based on its title, there's no need for
    a separate server name check.
    
    Also, the code was also causing every channel up until the one that
    matched the title to be made the current buffer of the view, as the
    server name of the channel and the server name of the buffer view would
    always match, but we only reset the '__unrestored__' object name on a
    title match. The result was that the channels were made visible, and
    hence would result in unread counts being reset.

--- src/app/splitview.cpp
+++ src/app/splitview.cpp
@@ -32,7 +32,6 @@
 #include "bufferview.h"
 #include "textbrowser.h"
 #include <QContextMenuEvent>
-#include <IrcBufferModel>
 #include <IrcConnection>
 #include <QApplication>
 #include <QScrollBar>
@@ -183,19 +182,14 @@ void SplitView::addBuffer(IrcBuffer* buffer)
         const QString uuid = connection->userData().value("uuid").toString();
         if (!uuid.isEmpty()) {
             // TODO: optimize
-            IrcBuffer* server = buffer->model()->get(0);
             QList<BufferView*> views = findChildren<BufferView*>("__unrestored__");
             foreach (BufferView* bv, views) {
-                bool tmatch = bv->property("__buffer__").toString() == buffer->title();
-                bool smatch = tmatch || bv->property("__server__").toString() == server->title();
-                if ((tmatch || smatch) && bv->property("__uuid__").toString() == uuid) {
+                if (bv->property("__uuid__").toString() == uuid &&
+                        bv->property("__buffer__").toString() == buffer->title()) {
                     bv->setBuffer(buffer);
-                    if (tmatch) {
-                        bv->setObjectName(QString());
-                        bv->setProperty("__buffer__", QVariant());
-                        bv->setProperty("__uuid__", QVariant());
-                        bv->setProperty("__server__", QVariant());
-                    }
+                    bv->setObjectName(QString());
+                    bv->setProperty("__buffer__", QVariant());
+                    bv->setProperty("__uuid__", QVariant());
                 }
             }
         }
@@ -308,8 +302,6 @@ QVariantMap SplitView::saveSplittedViews(const QSplitter* splitter) const
             buf.insert("buffer", buffer ? buffer->title() : QString());
             buf.insert("current", bv == d.current);
             buf.insert("uuid", connection ? connection->userData().value("uuid").toString() : QString());
-            IrcBuffer* server = buffer ? buffer->model()->get(0) : 0;
-            buf.insert("server", server ? server->title() : "");
             if (QSplitter* sp = bv->findChild<QSplitter*>())
                 buf.insert("state", sp->saveState());
             buf.insert("fontSize", bv->textBrowser()->font().pointSize());
@@ -353,7 +345,6 @@ void SplitView::restoreSplittedViews(QSplitter* splitter, const QVariantMap& sta
             QVariantMap buf = buffers.takeFirst().toMap();
             bv->setProperty("__buffer__", buf.value("buffer").toString());
             bv->setProperty("__uuid__", buf.value("uuid").toString());
-            bv->setProperty("__server__", buf.value("server").toString());
             if (buf.contains("state")) {
                 if (QSplitter* sp = bv->findChild<QSplitter*>())
                     sp->restoreState(buf.value("state").toByteArray());
