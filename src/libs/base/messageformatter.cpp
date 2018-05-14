communi/communi-desktop

commit 4209259c83c8b04472a8f6db168e36a880f05bc2

Fix handling of multi and non-hash types/prefixes in channel links
On some networks, such as freenode, channels may have multiple type
prefix characters (e.g. ##foobar). Furthermore, on some networks,
some types of channels (e.g. local server channels) have can have
different type prefix characters than the standard '#', such as '&'.

src/libs/base/messageformatter.cpp
@@ -81,6 +81,7 @@ void MessageFormatter::setBuffer(IrcBuffer* buffer)
     if (d.buffer != buffer) {
         d.buffer = buffer;
         d.userModel->setChannel(qobject_cast<IrcChannel*>(buffer));
+        d.channelTypes = buffer ? buffer->network()->channelTypes() : QStringList();
     }
 }
 
@@ -182,12 +183,12 @@ QString MessageFormatter::formatText(const QString& text) const
                         pos = end + 4;
                         continue;
                     }
-                } else if (c == '#') {
+                } else if (d.channelTypes.contains(c)) {
                     int end = pos + 1;
                     const int length = msg.length();
                     while (end < length) {
                         const QChar nextChar = msg.at(end);
-                        if (!nextChar.isLetterOrNumber() && (nextChar != '-') && (nextChar != '_'))
+                        if (!nextChar.isLetterOrNumber() && (nextChar != '-') && (nextChar != '_') && !d.channelTypes.contains(nextChar))
                             break;
                         ++end;
                     }

src/libs/base/messageformatter.h
@@ -109,6 +109,7 @@ private:
         IrcUserModel* userModel;
         IrcTextFormat* textFormat;
         QMultiHash<QChar, QString> names;
+        QStringList channelTypes;
     } d;
 };
 

src/libs/base/textbrowser.cpp
@@ -243,7 +243,7 @@ QMenu* TextBrowser::createContextMenu(const QPoint& pos)
         menu->insertAction(separator, joinAction);
         connect(joinAction, SIGNAL(triggered()), this, SLOT(onJoinTriggered()));
 
-        QString channel = QUrl(anchor).toString(QUrl::RemoveScheme | QUrl::FullyDecoded);
+        QString channel = anchor.mid(8);
 
         QAction* channelAction = new QAction(channel, menu);
         menu->insertAction(joinAction, channelAction);

commit ef287afdc0a879589f3b2c951574a56c06d75857

Take a step back with channel link detection
Simplify things and only care about "#" type channels. This is a partial
revert of commit 4209259c, which broke the formatted markup when it
contained "&" characters. For instance, "<" turned into a highlighted
"&lt;" and "&" turned into "&amp;".

src/libs/base/messageformatter.cpp
@@ -81,7 +81,6 @@ void MessageFormatter::setBuffer(IrcBuffer* buffer)
     if (d.buffer != buffer) {
         d.buffer = buffer;
         d.userModel->setChannel(qobject_cast<IrcChannel*>(buffer));
-        d.channelTypes = buffer ? buffer->network()->channelTypes() : QStringList();
     }
 }
 
@@ -183,12 +182,14 @@ QString MessageFormatter::formatText(const QString& text) const
                         pos = end + 4;
                         continue;
                     }
-                } else if (d.channelTypes.contains(c)) {
+                } else if (c == '#') {
                     int end = pos + 1;
                     const int length = msg.length();
+                    if (end < length && msg.at(end) == '#')
+                        ++end;
                     while (end < length) {
                         const QChar nextChar = msg.at(end);
-                        if (!nextChar.isLetterOrNumber() && (nextChar != '-') && (nextChar != '_') && !d.channelTypes.contains(nextChar))
+                        if (!nextChar.isLetterOrNumber() && (nextChar != '-') && (nextChar != '_'))
                             break;
                         ++end;
                     }

src/libs/base/messageformatter.h
@@ -109,7 +109,6 @@ private:
         IrcUserModel* userModel;
         IrcTextFormat* textFormat;
         QMultiHash<QChar, QString> names;
-        QStringList channelTypes;
     } d;
 };
 